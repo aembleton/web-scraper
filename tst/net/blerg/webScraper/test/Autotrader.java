659fc4bb32b26e49455f518da1278c6b1a96f2fb
Arthur Embleton <arthur@blerg.net>
2013-01-04 00:48:44 +0000

    Added try catch

--- tst/net/blerg/webScraper/test/Autotrader.java
+++ tst/net/blerg/webScraper/test/Autotrader.java
@@ -1,10 +1,14 @@
 package net.blerg.webScraper.test;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.List;
 
 import net.blerg.webScraper.WebScrape;
 
 import org.junit.Test;
+
+import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 
 public class Autotrader {
 
@@ -12,6 +16,11 @@ public class Autotrader {
 	public void scrapeCars() {
 		String startUrl="http://www.autotrader.co.uk/search/used/cars/postcode/l351rz/radius/1/onesearchad/used%2Cnearlynew%2Cnew/quicksearch/true";
 		
-		List<String> w = new WebScrape(startUrl).scrape(String.class);
+		try {
+			List<String> w = new WebScrape(startUrl).scrape(String.class);
+		} catch (FailingHttpStatusCodeException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
