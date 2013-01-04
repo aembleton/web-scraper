daed79c9fc6112b832bce72ac047422c07a4a982
Arthur Embleton <arthur@blerg.net>
2013-01-04 00:48:06 +0000

    Some getting by xpath

--- src/net/blerg/webScraper/WebScrape.java
+++ src/net/blerg/webScraper/WebScrape.java
@@ -1,7 +1,14 @@
 package net.blerg.webScraper;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.LinkedList;
 import java.util.List;
+
+import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.DomNode;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
 public class WebScrape {
 	
@@ -23,7 +30,12 @@ public class WebScrape {
 		return this;
 	}
 	
-	public <T> List<T> scrape(Class<T> type) {
+	public <T> List<T> scrape(Class<T> type) throws FailingHttpStatusCodeException, MalformedURLException, IOException {
+		
+		WebClient webClient = new WebClient();
+		HtmlPage page = webClient.getPage(startUrl);
+		List<?> links=page.getByXPath(linkXpath);
+		
 		List<T> list = new LinkedList<>();
 		return list;
 	}
